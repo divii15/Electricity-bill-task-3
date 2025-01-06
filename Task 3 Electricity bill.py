947216b504c312525660e344d50d50513de018b5
divii15 <divyadaksha143@gmail.com>
2025-01-06 17:33:43 +0530

    My next task(3)(8)

--- Task 3 Electricity bill.py
+++ Task 3 Electricity bill.py
@@ -1,3 +1,17 @@
+#TASK 3
+#8)Nexted if:
+#Get the Electrycity UNIT from user.
+#if unit is upto 199----charge is 1.20 Rs
+#if unit is 200 and above but less than 400----charge is 1.50 Rs
+#if unit is 600 and above ----charge is 2.00 Rs.
+#find bill.
+#suppose bill exceed RS. 400 the subcharge of 15% will be charged and minimum bill be should be RS. 100
+#ex:
+#input unit:300
+#output:3450
+
+
+
 units = int(input("Enter the units consumed:"))
 if units>1 and units<=199:
     charge=1.20
